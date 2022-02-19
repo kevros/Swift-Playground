5426c783763ce1e154c875acc7c0ff930ec884cd
gm.szopa@gmail.com <gm.szopa@gmail.com>
2022-02-19 18:15:07 +0100

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Grzegorz Szopa on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
